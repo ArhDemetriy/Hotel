349915d58df2ef836a98f188f93930470bcb9807
Demetriy Nazimov <ArhDemetriy@GMail.com>
2020-07-07 18:53:46 +0800

    перенос датапикера в соответсвующий ему блок

--- src/components/text_field/text_field.js
+++ src/components/text_field/text_field.js
@@ -1,21 +1,3 @@
-$('.datepicker-here').datepicker({
-  clearButton: true,
-  setButton: true,
-  altFieldDateFormat: 'dd.mm.yyyy',
-  multipleDatesSeparator: ' - ',
-  prevHtml: '<span class="datepicker__nav_arrow material-icons-outlined">arrow_back</span>',
-  nextHtml: '<span class="datepicker__nav_arrow material-icons-outlined">arrow_forward</span>',
-  navTitles: {
-      days: 'MM yyyy'
-  },
-  onRenderCell: function (date, cellType) {
-    var currentDate = date.getDate();
+'use strict';{
 
-    // Добавляем вспомогательный элемент, если число содержится в `eventDates`
-    if (cellType == 'day') {
-        return {
-            html: '<span class="datepicker--day">' + currentDate + '</span>'
-        }
-    }
-  },
-})
+}
