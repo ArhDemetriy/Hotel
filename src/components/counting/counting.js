f81b6d5914b77306e7776dcf4de7282970fce0a7
Demetriy Nazimov <ArhDemetriy@GMail.com>
2020-07-11 11:06:07 +0800

    перенёс counting на buton & input

--- src/components/counting/counting.js
+++ src/components/counting/counting.js
@@ -1,10 +1,9 @@
 'use strict'; {
-  function handler(event) {
-
+  function changingValue(event) {
     const shiftValue = (() => {
-      if (event.target.matches('.counting__inc'))
+      if (event.target.matches('.counting__inc:not(:disabled)'))
         return 1
-      else if (event.target.matches('.counting__dec:not(.counting__button-deactive)'))
+      else if (event.target.matches('.counting__dec:not(:disabled)'))
         return -1
       else
         return 0
@@ -12,28 +11,36 @@
     if (shiftValue == 0) return;
     const
       iterator = this.querySelector('.counting__iterator'),
-      iteratorValue = iterator.textContent,
-      countValue = this.dataset.count;
+      iteratorValue = iterator.value;
 
     let newValue = 0;
-    if (isFinite(countValue))
-      newValue = Number(countValue)
-    else if (isFinite(iteratorValue))
+    if (isFinite(iteratorValue))
       newValue = Number(iteratorValue);
     newValue += shiftValue;
-    if (newValue < 0) // проверка нужна на случай невалидных данных и клике по .counting__dec:not(.counting__button-deactive)
+    if (newValue < 0) // проверка нужна на случай невалидных данных и клике по .counting__dec:not(:disabled)
       newValue = 0;
 
-    if (newValue == 0)
-      this.querySelector('.counting__dec').classList.add("counting__button-deactive")
-    else if (newValue == 1)
-      this.querySelector('.counting__dec').classList.remove("counting__button-deactive")
+    if (newValue < 1)
+      this.querySelector('.counting__dec').disabled = true;
+    else if (newValue >= 1)
+      this.querySelector('.counting__dec').disabled = false;
 
-    iterator.textContent = this.dataset.count = newValue;
+    iterator.value = newValue;
+    }
+  function settingButtonStage(event) {
+    if (!event.target.matches('.counting__iterator')) return;
+    const value = event.target.value;
+    if (!value) return;
+
+    if (value < 1)
+      this.querySelector('.counting__dec').disabled = true;
+    else if (value >= 1)
+      this.querySelector('.counting__dec').disabled = false;
   }
 
   const elements = document.querySelectorAll(`.counting`);
   elements.forEach((_, i, a) => {
-    a[i].addEventListener('click', handler, { passive: true, });
+    a[i].addEventListener('click', changingValue, { passive: true, });
+    a[i].addEventListener('change', settingButtonStage, { passive: true, });
   });
 }
