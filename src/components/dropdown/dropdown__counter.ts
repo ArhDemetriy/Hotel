0730c030ab3300853168e85a840a75fc956b2984
ArhDemetriy <ArhDemetriy@GMail.com>
2020-10-29 11:02:00 +0800

    in dropdown_counters.ts fix incorrect bind event handlers del commented code ref button.disabled cange in setTimeout add import isTrulyFinite function

--- src/components/dropdown/dropdown__counter.ts
+++ src/components/dropdown/dropdown__counter.ts
@@ -1,3 +1,4 @@
+import { isTrulyFinite } from '../global_mixins/global_mixins'
 export { iCounter, iClearebleCounter, fabricEventListenersForCounters }
 
 interface iCounter {
@@ -18,46 +19,43 @@ function fabricEventListenersForCounters(counters: iCounter[]) {
     }
     const setValue = (iterator: HTMLInputElement, value: string, oppositeButton: HTMLButtonElement) => {
       iterator.value = value;
-      if (oppositeButton.disabled)
-        oppositeButton.disabled = false;
+      setTimeout((oppositeButton: HTMLButtonElement) => {
+        if (oppositeButton.disabled)
+          oppositeButton.disabled = false;
+       }, 0, oppositeButton);
     }
     function increment(this: iCounter) {
       const value = getValue(this.iterator.value, this.iterator.min);
-      const max = isFinite(+this.iterator.max) ? +this.iterator.max : Infinity;
+      const max = this.iterator.max && isFinite(+this.iterator.max) ? +this.iterator.max : Infinity;
       if (value < max - 1)
         setValue(this.iterator, String(value + 1), this.decrementButton)
-        // setTimeout(setValue, 0, this.iterator, String(value + 1), this.decrementButton)
       else {
         this.incrementButton.disabled = true;
         if (value != max)
           setValue(this.iterator, String(max), this.decrementButton)
-          // setTimeout(setValue, 0, this.iterator, String(max), this.decrementButton)
       }
     }
     function decrement(this: iCounter) {
       const value = getValue(this.iterator.value, this.iterator.min);
-      const min = isFinite(+this.iterator.min) ? +this.iterator.min : 0;
+      const min = this.iterator.min && isFinite(+this.iterator.min) ? +this.iterator.min : 0;
       if (value > min + 1)
         setValue(this.iterator, String(value - 1), this.incrementButton)
-        // setTimeout(setValue, 0, this.iterator, String(value - 1), this.incrementButton)
       else {
         this.decrementButton.disabled = true;
         if (value != min)
           setValue(this.iterator, String(min), this.incrementButton)
-          // setTimeout(setValue, 0, this.iterator, String(min), this.incrementButton)
       }
     }
     return counters.map(counter => {
-      const bindedIncrement = increment.bind(counter);
-      const bindedDecrement = decrement.bind(counter);
-      const clearebleCounter: iClearebleCounter = Object.assign(counter, {
-        removeEventListeners: function (this: iCounter) {
+      const clearebleCounter: iClearebleCounter = Object.assign(counter);
+      const bindedIncrement = increment.bind(clearebleCounter);
+      const bindedDecrement = decrement.bind(clearebleCounter);
+      clearebleCounter.removeEventListeners = function (this: iCounter) {
           this.incrementButton.removeEventListener('click', bindedIncrement);
           this.decrementButton.removeEventListener('click', bindedDecrement);
         }
-      })
       clearebleCounter.incrementButton.addEventListener('click', bindedIncrement, { passive: true });
       clearebleCounter.decrementButton.addEventListener('click', bindedDecrement, { passive: true });
       return clearebleCounter;
     });
-  }
+}
